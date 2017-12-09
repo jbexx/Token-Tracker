7328860e6f63745fc2a9098fb2fac2270f3af1b6
Jonathan Beckman <jbecks.media@gmail.com>
2017-12-09 13:22:22 -0700

    clean up for merge with master

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -21,11 +21,6 @@ export default class Search extends Component {
     this.searchTokens = this.searchTokens.bind(this);
   }
 
-  // create an action that will update cryptodata in store
-  // with filtered results from this.trie.suggest()
-  // want action to send updated / filtered array of store
-  // and then set that new array to store as the new cryptodata
-
   componentWillReceiveProps(nextprops) {
     const trie = new Trie()
     trie.populate(
@@ -35,54 +30,30 @@ export default class Search extends Component {
   }
 
   searchTokens = () => {
-    const { CryptoData } = this.props;
+    const { CryptoData, updateStore } = this.props;
     const { suggestions } = this.state;
-    let newData = [];
+    const newData = [];
     let i1 = 0;
     let i2 = 0;
-    let counter = 0;
     const i1Length = CryptoData.length;
     const i2Length = suggestions.length;
-    console.log('i2 length out of while', i2Length)
-    
 
-    while (i1 < i1Length && i2Length ) {
-      counter++
-      // console.log('i1 ', i1)
-      console.log('i2 ', i2)
-      // console.log('the array ', CryptoData[100])
-      // console.log('the token ', CryptoData[i1].name.toLowerCase())
-      console.log('i2 length', i2Length)
-      // console.log('newdata length', newData.length)
-      // console.log('undefined? ', i1Length)
-      console.log('sgstn at i2 ', suggestions[i2].toLowerCase())
+    while (i1 < i1Length && i2Length) {
 
       while (CryptoData[i1].name.toLowerCase() !== suggestions[i2].toLowerCase()) {
-        i2++
-        if (i2 > i2Length) {
-          i1++
-          i2 = 0
-        }
-        break;
+        i1++
       }
       
-      while (CryptoData[i1].name.toLowerCase() === suggestions[i2].toLowerCase()) {
+      if (CryptoData[i1].name.toLowerCase() === suggestions[i2].toLowerCase()) {
         newData.push(CryptoData[i1])
-        i1++
-        i2 = 0
-        break;
+        i1 = 0
+        i2++
       }
 
       if (newData.length === i2Length) {
-      console.log('newdata length when return', newData.length)
-      
-        console.log({counter})
-        return newData
+        return updateStore(newData)
       }
-    }
-
-    // console.log({newData})
-    // this.props.updateStore(newData)    
+    } 
   }
 
   updateTokens = value => {
@@ -91,26 +62,7 @@ export default class Search extends Component {
       text: value,
       suggestions
     },
-    () => {
-      console.log('search tokens ', this.searchTokens())})
-
-    // if (this.state.suggestions.length) {
-    //   console.log('search tokens ', this.searchTokens())
-    // }
-    
-    
-
-
-
-    // for (let j = 0; j < this.props.CryptoData.length; j++) {
-    //   for (let i = 0; i < this.state.suggestions.length; i++) {
-    //     if (this.props.CryptoData[j].name.toLowerCase() === this.state.suggestions[i].toLowerCase()) {
-    //       newData.push(this.props.CryptoData[j])
-    //     }
-    //   }
-    // }
-
-    // this.props.updateStore(newData)
+    () => this.searchTokens())
   }
 
   render() {
